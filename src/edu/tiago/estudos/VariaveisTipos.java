f8bd72212a7e171477bb0279b1f68bbe216f5ce9
TiagoCarvalho-true <carvalhost58@gmail.com>
2025-03-23 13:17:51 -0400

    atualizando repositorios

--- src/edu/tiago/estudos/VariaveisTipos.java
+++ src/edu/tiago/estudos/VariaveisTipos.java
@@ -1,5 +1,11 @@
 package edu.tiago.estudos;
 
 public class VariaveisTipos {
+    int numeroInteiro = 10;         // Variável de instância
+    static double valorEstático = 99.99; // Variável estática
     
+    void exemploLocal() {
+        String mensagem = "Olá, Java!"; // Variável local
+        System.out.println(mensagem);
+    }
 }
